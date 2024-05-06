af824d644462c5266ed4a55e1361ced7a7a7b6b7
hcmh330 <hcmh330@gmail.com>
2024-05-06 14:24:49 +0100

    update

---
+++ app/Http/Controllers/user/ProfilController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\user;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfilController extends Controller
+{
+    public function index(){
+        return view('user.profil');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\user\ProfilController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+
+Route::get('/', function () {
+    return view('/home');
+});
+
+Auth::routes(["verify"=>true]);
+// Route::get('/user/profil', [ProfilController::class, 'index'])->name('user.profil');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/user/profil', [App\Http\Controllers\user\ProfilController::class, 'index'])->name('user.profil')->middleware('auth');
